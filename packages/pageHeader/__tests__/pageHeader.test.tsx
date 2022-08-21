389e9c832b9a15002833e6f0eb1a1fb135eaef9d
Ranielli <raniellimontagna@hotmail.com>
2022-08-21 13:52:07 -0300

    test(pageHeader): implementa testes no componente pageHeader

---
+++ packages/pageHeader/__tests__/pageHeader.test.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { axe } from 'jest-axe';
+
+import { PageHeader } from '../src';
+import { ThemeProvider } from '../../themeProvider/lib';
+
+it('is accessible', async () => {
+  const { container } = render(
+    <ThemeProvider>
+      <PageHeader
+        titulo="teste"
+        button={{
+          children: 'teste',
+        }}
+        right={<>espaço na direita</>}
+      />
+    </ThemeProvider>
+  );
+  expect(await axe(container)).toHaveNoViolations();
+});
--- packages/pageHeader/src/pageHeader.styles.ts
+++ packages/pageHeader/src/pageHeader.styles.ts
@@ -37,8 +37,8 @@ export const DivPageHeader = styled.div`
       width: 100%;
       height: 100%;
       border-radius: 100%;
-      background-color: ${({ theme }) => theme?.cores?.secundaria || 'white'};
-      color: ${({ theme }) => theme?.cores?.terciaria || 'black'};
+      background-color: ${({ theme }) => theme.cores.secundaria};
+      color: ${({ theme }) => theme.cores.terciaria};
       border: none;
       box-shadow: 0px 1px 5px 1px rgba(0, 0, 0, 0.5);
     }
