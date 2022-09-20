30a2324f6aa6dcc1f0c62efcc5f245a36b510a32
Ranielli <raniellimontagna@hotmail.com>
2022-09-20 15:37:38 -0300

    refactor(fields): ajusta exportação dos fields

--- packages/fields/src/index.ts
+++ packages/fields/src/index.ts
@@ -3,3 +3,6 @@ export { NumberField } from './numberField/numberField';
 export { SearchField } from './searchField/searchField';
 export { TextField } from './textField/textField';
 export { DatePicker } from './datePicker/datePicker';
+export { Creatable } from './creatable/creatable';
+export { Select } from './select/select';
+export { Autocomplete } from './autoComplete/autoComplete';
