8b135998c479d03e82f5e84654f10bfed4ed08ce
Ranielli <raniellimontagna@hotmail.com>
2022-10-01 15:15:29 -0300

    feat(fields): implementa mascaras e melhora componente `textField`

--- packages/storybook/src/stories/fields/textField.stories.tsx
+++ packages/storybook/src/stories/fields/textField.stories.tsx
@@ -1,5 +1,6 @@
+import { Button, Grid } from '@mui/material';
 import { Form } from '../../../../fields/src/form';
-import { TextField } from '../../../../fields/src/textField/textField';
+import TextField from '../../../../fields/src/textField/textField';
 
 export default {
   component: TextField,
@@ -9,11 +10,46 @@ export default {
 export const textField = () => {
   return (
     <Form
-      onSubmit={() => {
-        //
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      onSubmit={(values: any) => {
+        /* eslint-disable no-console */
+        console.log(values);
       }}
     >
-      <TextField name="nome" />
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <TextField name="semMascara" label="Sem máscara" />
+        </Grid>
+        <Grid item xs={6}>
+          <TextField name="cpf" label="CPF" mask="cpf" />
+        </Grid>
+        <Grid item xs={6}>
+          <TextField name="cnpj" label="CNPJ" mask="cnpj" />
+        </Grid>
+        <Grid item xs={6}>
+          <TextField name="cpfCnpj" label="CPF/CNPJ" mask="cpfCnpj" />
+        </Grid>
+        <Grid item xs={6}>
+          <TextField name="cep" label="CEP" mask="cep" />
+        </Grid>
+        <Grid item xs={6}>
+          <TextField name="telefone" label="Telefone" mask="telefone" />
+        </Grid>
+        <Grid item xs={6}>
+          <TextField name="decimal" label="Decimal" mask="decimal" />
+        </Grid>
+        <Grid item xs={6}>
+          <TextField name="numero" label="Numero" mask="numero" />
+        </Grid>
+        <Grid item xs={6}>
+          <TextField name="valor" label="Valor" mask="valor" />
+        </Grid>
+        <Grid item xs={12}>
+          <Button type="submit" variant="contained" color="primary">
+            Submit
+          </Button>
+        </Grid>
+      </Grid>
     </Form>
   );
 };
