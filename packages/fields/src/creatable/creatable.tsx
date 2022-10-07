ac5eb1a8c31680a07f6a5f3b35401070fb6dca56
Ranielli <raniellimontagna@hotmail.com>
2022-10-06 22:56:35 -0300

    refactor(fields): adiciona value personalizavel

--- packages/fields/src/creatable/creatable.tsx
+++ packages/fields/src/creatable/creatable.tsx
@@ -46,6 +46,7 @@ export const Creatable = ({
             <Autocomplete
               size="small"
               fullWidth
+              value={rest?.value || undefined}
               onChange={(_, values) => onChange(values)}
               options={options}
               noOptionsText={
